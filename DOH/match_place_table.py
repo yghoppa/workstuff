f4e38dfadf73118aa98b7032a0077f791ca8d731
yghoppa <jomelle.wong@obf.ateneo.edu>
2022-01-25 17:59:58 +0800

    added renaming dict; province join done; adding huc join.

--- DOH/match_place_table.py
+++ DOH/match_place_table.py
@@ -8,6 +8,18 @@ import pandas as pd
 datafilename = "datafile_input.xlsx"
 outfilename = "output.xlsx"
 
+#   Some place names from the FHSIS do not match the PCT table.
+#   Make dictionary/hash to automate name swap.
+rename_dict = {
+    'COTABATO': 'COTABATO (NORTH COTABATO)',
+    'PROVINCE OF DINAGAT': 'DINAGAT ISLANDS',
+    'NORTHERN LEYTE': 'LEYTE',
+    'MT. PROVINCE': 'MOUNTAIN PROVINCE',
+    'MINDORO OCCIDENTAL': 'OCCIDENTAL MINDORO',
+    'MINDORO ORIENTAL': 'ORIENTAL MINDORO',
+    'WESTERN SAMAR': 'SAMAR (WESTERN SAMAR)'
+}
+
 #   Loading entire place_table into a DataFrame.
 #   Add new columns for variables/indicators then write to an out file.
 output_table = pd.read_excel("place_table.xlsx", "Sheet1")
@@ -15,15 +27,19 @@ output_table = pd.read_excel("place_table.xlsx", "Sheet1")
 #   Loading entire data file (pdf-to-excel file of FHSIS) into a DataFrame.
 #   Match row place name to output_table (place_table) then add data.
 data_table = pd.read_excel(datafilename, "Table 1")
+data_table = data_table.set_index('AREA')
 
 #   Selecting "province" rows, because province name repeats in place_table.
 #   Will do the same with HUCs, etc. Set index to province name and
 #   use join(). Store copy of original index and restore later to merge
 #   with output_table.
 prov_df = output_table.loc[output_table['CATEGORY'] == 'Province']
-temp_df = pd.DataFrame(index=prov_df.index.copy())
-#prov_df = prov_df.set_index('Province').join(data_table.set_index('AREA'))
+prov_df = prov_df.join(data_table, on='Province')
+output_table = output_table.join(prov_df, rsuffix='_in')
+temp = output_table['ALT_LOCAL']
+output_table['ALT_LOCAL'] = temp.str.upper()
+output_table.update(data_table.rename(columns={'AREA': 'ALT_LOCAL'}))
 
-[data_table[place == prov_df['Province']] for place in data_table['AREA']]
+output_table.to_excel(outfilename)
 
-#prov_df.to_excel(outfilename)
+print(output_table.columns)
